132c17fd671b068d8e83ec5c49598c35a467e94b
Kaushal Vaid <kaushal00vaid@gmail.com>
2025-04-22 22:09:56 +0530

    week-3 middlewares done.

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -1,6 +1,6 @@
-const request = require('supertest');
-const assert = require('assert');
-const express = require('express');
+const request = require("supertest");
+const assert = require("assert");
+const express = require("express");
 const app = express();
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
@@ -12,16 +12,35 @@ const app = express();
 // clears every one second
 
 let numberOfRequestsForUser = {};
-setInterval(() => {
-    numberOfRequestsForUser = {};
-}, 1000)
+const interval = setInterval(() => {
+  numberOfRequestsForUser = {};
+}, 1000);
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+function ratelimit(req, res, next) {
+  const userId = req.headers["user-id"];
+  numberOfRequestsForUser[userId] = numberOfRequestsForUser[userId] + 1 || 1;
+
+  if (numberOfRequestsForUser[userId] > 5) {
+    return res.status(404).json({ msg: "Rate limit exceeded" });
+  }
+
+  next();
+}
+
+app.use(ratelimit);
+
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
 });
+
+// Clean up the interval when the process exits (Jest finishes tests)
+process.on("exit", () => clearInterval(interval));
+process.on("SIGINT", () => clearInterval(interval));
+process.on("SIGTERM", () => clearInterval(interval)); // for Jest environments
+interval.unref();
 
 module.exports = app;
